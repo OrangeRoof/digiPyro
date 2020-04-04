2835b2434a08573d17f25f6284f62b0046c686d7
Dj <davidabraham@ucla.edu>
2020-04-04 16:40:48 -0700

    added cli for synths.py

--- synths.py
+++ synths.py
@@ -1,3 +1,31 @@
+import argparse as ap
+import numpy as np
+
+parser = ap.ArgumentParser(description = 'This program creates a synthetic .avi movie for use with DigiPyRo.')
+
+parser.add_argument('-t', '--time', type=float, default=5,
+                    help='The desired length of the movie in seconds.')
+parser.add_argument('-f', '--fps', type=float, default=30.0,
+                    help='The frame rate of the video (frames per second).')
+parser.add_argument('-w', '--width', type=int, default=1260,
+                    help='The width in pixels of the video.')
+parser.add_argument('-l', '--length', type=int, default=720,
+                    help='The height in pixels of the video.')
+parser.add_argument('-r', '--rpm', type=float, default=10.0,
+                    help='The frequency of oscillations.')
+parser.add_argument('-R', '--rotRate', type=float, default=0.0,
+                    help='The rotation rate of the camera. The two natural frames of reference are with rotRate = and rotRate = rpm.')
+parser.add_argument('--r0', type=float, default=1.0,
+                    help='The initial radial position of the particle.')
+parser.add_argument('--vr0', type=float, default=0.0,
+                    help='The initial radial velocity of the particle.')
+parser.add_argument('--phi0', type=float, default=np.pi/4,
+                    help='The initial azimuthal position of the particle. Choose a value between 0 and 2*pi.')
+parser.add_argument('--vphi0', type=float, default = 0,
+                    help='The initial azimuthal velocity of the particle.')
+
+args = parser.parse_args()
+
 # This program creates a synthetic .avi movie for use with DigiPyRo
 # The video shows a ball rolling on a parabolic surface
 # The user may change the length of the movie[1], the frame rate of the movie[2], the resolution of the movie[3]
@@ -6,7 +34,6 @@
 
 # Import necessary modules
 import cv2
-import numpy as np
 from tkinter import *
 import matplotlib
 matplotlib.use("Agg")
@@ -17,26 +44,27 @@ spinlab = cv2.imread('SpinLabUCLA_BW_strokes.png') # spinlab logo to display in 
 
 
 # Define movie details
-movLength = 5                           # [1] define the desired length of the movie in seconds
-fps = 30.0                              # [2] Set this to a low value (10-15) for increased speed or a higher value (30-60) for better results with DigiPyRo
-width = 1260                            # [3] Width and height in pixels
-height = 720                            # [3] Decrease the width and height for increased speed, increase for improved resolution
+movLength = args.time                           # [1] define the desired length of the movie in seconds
+fps = args.fps                              # [2] Set this to a low value (10-15) for increased speed or a higher value (30-60) for better results with DigiPyRo
+width = args.width                            # [3] Width and height in pixels
+height = args.length                            # [3] Decrease the width and height for increased speed, increase for improved resolution
 spinlab = cv2.resize(spinlab,(int(0.2*width),int((0.2*height)/3)), interpolation = cv2.INTER_CUBIC)
 
 
 # Define table values
-rpm = 10.0                              # [4] frequency of oscillations (in RPM). Good values might be 5-15
+rpm = args.rpm                              # [4] frequency of oscillations (in RPM). Good values might be 5-15
                                         # NOTE: A two-dimensional rotating system naturally takes the shape of a parabola.
                                         # The rotation rate determines the curvature of the parabola, which is why we define the curvature in terms of RPM
-rotRate = 0.0                          # [5] rotation rate of camera. The two natural frames of reference are with rotRate = 0 and rotRate = rpm
+rotRate = args.rotRate                          # [5] rotation rate of camera. The two natural frames of reference are with rotRate = 0 and rotRate = rpm
 
 
 # Set initial conditions
-r0 = 1.0                                # [6] initial radial position of ball. Choose a value between 0 and 1
-vr0 = 0.0                               # [7] initial radial velocity of ball. Good values might be 0-1
-phi0 = np.pi/4                          # [8] initial azimuthal position of ball. Choose a value between 0 and 2*pi
-vphi0 = 0.0                             # [9] initial azimuthal velocity of ball. Good values might be 0-1
+r0 = args.r0                                # [6] initial radial position of ball. Choose a value between 0 and 1
+vr0 = args.vr0                               # [7] initial radial velocity of ball. Good values might be 0-1
+phi0 = args.phi0                          # [8] initial azimuthal position of ball. Choose a value between 0 and 2*pi
+vphi0 = args.vphi0                             # [9] initial azimuthal velocity of ball. Good values might be 0-1
 
+print(r0,vr0,phi0,vphi0)
 
 # Ask user for movie name
 saveFile = input('Enter a name for the movie (e.g. mySyntheticMovie): ')

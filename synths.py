9ba92d4c819087ddae1fb59b8440b802b2d94a2e
Dj <davidabraham@ucla.edu>
2020-04-10 17:31:21 -0700

    updated help message

--- synths.py
+++ synths.py
@@ -23,29 +23,38 @@ parser = ap.ArgumentParser(description = msg,
 parser.add_argument('-t', '--time', type=float, default=5,
                     help='The desired length of the movie in seconds.')
 parser.add_argument('-f', '--fps', type=float, default=30.0,
-                    help='The frame rate of the video (frames per second).')
+                    help=('The frame rate of the video (frames per second). '
+                          'Set this to a low value (10-15) for increased speed'
+                          ' or a higher value (30-60) for better results.'))
 parser.add_argument('-w', '--width', type=int, default=1260,
                     help='The width in pixels of the video.')
 parser.add_argument('-l', '--length', type=int, default=720,
                     help='The height in pixels of the video.')
 parser.add_argument('-r', '--rpm', type=float, default=10.0,
-                    help='The frequency of oscillations.')
+                    help=('The frequency of oscillations. A good value '
+                          'would be between 5 and 15.'))
 parser.add_argument('-R', '--rotRate', type=float, default=0.0,
-                    help='The rotation rate of the camera. The two natural frames of reference are with rotRate = and rotRate = rpm.')
+                    help=('The rotation rate of the camera. The two natural '
+                          'frames of reference are with rotRate = 0 and '
+                          'rotRate = rpm.'))
 parser.add_argument('--r0', type=float, default=1.0,
-                    help='The initial radial position of the ball.')
+                    help=('The initial radial position of the ball. Choose a '
+                          'value betweeon 0 and 1.'))
 parser.add_argument('--vr0', type=float, default=0.0,
-                    help='The initial radial velocity of the ball.')
+                    help=('The initial radial velocity of the ball. '
+                          'A good value would be between 0 and 1.'))
 parser.add_argument('--phi0', type=float, default=np.pi/4,
-                    help='The initial azimuthal position of the ball. Choose a value between 0 and 2*pi.')
+                    help=('The initial azimuthal position of the ball. '
+                          'Choose a value between 0 and 2*pi.'))
 parser.add_argument('--vphi0', type=float, default = 0,
-                    help='The initial azimuthal velocity of the ball.')
+                    help=('The initial azimuthal velocity of the ball. '
+                          'A good value would be between 0 and 1.'))
 
 # collecting user input into list
 args = parser.parse_args()
 
 
-spinlab = cv2.imread('SpinLabUCLA_BW_strokes.png') # spinlab logo to display in upper right corner of output video
+spinlab = cv2.imread('util/SpinLabUCLA_BW_strokes.png') # spinlab logo to display in upper right corner of output video
 
 
 # Define movie details

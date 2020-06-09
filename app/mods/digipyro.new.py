7adc2bb92cc59d06a98eebe904e1b2db4620af02
dj-2805 <davidabraham@ucla.edu>
2020-06-08 18:06:02 -0700

    have script to check rotation setup

--- app/mods/digipyro.new.py
+++ app/mods/digipyro.new.py
@@ -8,10 +8,14 @@ x,y = 10,10
 center = (x,y)
 rpm = 10
 
+fileName = 'check.mp4'
+vid = cv2.VideoCapture(filename)
+width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-
+fileName = 'checkRot'
 fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
-video_writer = cv2.VideoWriter(fileName+'.mp4', fourcc, fps, )
+video_writer = cv2.VideoWriter(fileName+'.mp4', fourcc, fps, (width, height))
 
 numFrames = int(fps*(timeFinish - timeStart))
 
@@ -19,4 +23,12 @@ dtheta = 2*np.pi*rpm/(fps*60)
 # dtheta = 6*rpm/fps
 
 for i in range(numFrames):
+    ret, frame = vid.read()
+    frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_CUBIC)
+
     M = cv2.getRotationMatrix2D(center, i*dtheta, 1.0)
+    frame = cv2.warpAffine(frame, M, (width, height))
+    centered = cv2.resize(frame, (width, height), interpolation=cv2.INTER_CUBIC)
+    video_writer.write(frame)
+
+video_writer.release()
